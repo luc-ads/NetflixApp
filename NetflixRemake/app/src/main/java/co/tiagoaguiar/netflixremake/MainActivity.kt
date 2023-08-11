3cdf20fc8086a8a551d68049907b5dd59d56c988
luc-ads <lucas.psilva@bandtec.com.br>
2023-08-10 22:14:53 -0300

    Inserindo layout para a recycler

--- NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
+++ NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -2,8 +2,11 @@ package co.tiagoaguiar.netflixremake
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.netflixremake.adapters.MainAdapter
 import co.tiagoaguiar.netflixremake.databinding.ActivityMainBinding
+import co.tiagoaguiar.netflixremake.model.Movie
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.rv.adapter = MainAdapter()
+        val filmes = mutableListOf<Movie>()
+
+        for (i in 0 until 20) {
+            filmes.add(
+                Movie(R.drawable.movie)
+            )
+        }
+
+        binding.rv.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        binding.rv.adapter = MainAdapter(filmes)
     }
 }
--- NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/adapters/MainAdapter.kt
+++ NetflixRemake/app/src/main/java/co/tiagoaguiar/netflixremake/adapters/MainAdapter.kt
@@ -2,10 +2,15 @@ package co.tiagoaguiar.netflixremake.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import co.tiagoaguiar.netflixremake.databinding.MovieItemBinding
+import co.tiagoaguiar.netflixremake.model.Movie
 
-class MainAdapter: RecyclerView.Adapter<MainAdapter.MovieViewHolder>() {
+class MainAdapter(
+    private val listMovie: List<Movie>
+): RecyclerView.Adapter<MainAdapter.MovieViewHolder>() {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -17,11 +22,17 @@ class MainAdapter: RecyclerView.Adapter<MainAdapter.MovieViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: MainAdapter.MovieViewHolder, position: Int) {
+        holder.bind(listMovie[position])
     }
 
-    override fun getItemCount(): Int = 20
+    override fun getItemCount(): Int = listMovie.size
 
     inner class MovieViewHolder(itemBinding: MovieItemBinding) : RecyclerView.ViewHolder(itemBinding.root) {
 
+        private val textMovieName: ImageView = itemBinding.movieTitle
+
+        fun bind(itemMovie: Movie) {
+            textMovieName.setImageResource(itemMovie.coverUrl)
+        }
     }
 }
